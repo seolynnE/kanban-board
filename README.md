b58e9b316d123a0f1848fd53c21233eeeee181b6
seolynnE <tjfls2004@gmail.com>
2024-02-02 02:19:56 +0900

    드래그&핸들기능

--- README.md
+++ README.md
@@ -21,3 +21,22 @@
    단, child는 react요소가 아닌 함수로 구현해야 한다.
 3. `Draggable`사용 시 draggableId props, child, index 필수.
    단, child는 react요소가 아닌 함수로 구현해야 한다.
+
+### beautiful-dnd에서 주는 props를 받아와야 한다.
+
+```
+<Droppable droppableId="one">
+          {(magic) => (
+            <ul ref={magic.innerRef} {...magic.droppableProps}>
+              <Draggable draggableId="first" index={0}>
+                {(magic) => <li ref={magic.innerRef} {...magic.draggableProps} {...magic.dragHandleProps}>one</li>}
+              </Draggable>
+              <Draggable draggableId="first" index={1}>
+                {() => <li>two</li>}
+              </Draggable>
+            </ul>
+          )}
+        </Droppable>
+```
+
+- li에 magic의 props들을 추가하면 모든게 li에 적용된다
--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,45 @@
 import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
-
+import styled from "styled-components";
+const Wrap = styled.div`
+  li {
+    margin: 10px 0;
+    font-size: 20px;
+  }
+`;
 function App() {
   const onDragEnd = () => {};
   return (
     <DragDropContext onDragEnd={onDragEnd}>
-      <div>
+      <Wrap>
         <Droppable droppableId="one">
-          {() => (
-            <ul>
-              <Draggable>{() => <li></li>}</Draggable>
+          {(magic) => (
+            <ul ref={magic.innerRef} {...magic.droppableProps}>
+              <Draggable draggableId="first" index={0}>
+                {(magic) => (
+                  <li
+                    ref={magic.innerRef}
+                    {...magic.draggableProps}
+                    {...magic.dragHandleProps}
+                  >
+                    one
+                  </li>
+                )}
+              </Draggable>
+              <Draggable draggableId="second" index={1}>
+                {(magic) => (
+                  <li
+                    ref={magic.innerRef}
+                    {...magic.draggableProps}
+                    {...magic.dragHandleProps}
+                  >
+                    two!
+                  </li>
+                )}
+              </Draggable>
             </ul>
           )}
         </Droppable>
-      </div>
+      </Wrap>
     </DragDropContext>
   );
 }
